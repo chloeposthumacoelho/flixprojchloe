5ea4052c955b730a573856c14290882ae4326790
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2022-09-10 22:32:57 -0400

    Add files via upload

---
+++ project1flixchloe/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Assignment1
+//
+//   Created by Chloe Posthuma-Coelho on 9/10/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ project1flixchloe/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Assignment1
+//
+//  Created by Chloe Posthuma-Coelho on 9/10/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
